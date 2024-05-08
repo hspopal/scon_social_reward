beb8e189a1eb12b7ff052ccdc0b5f06a1238217d
hspopal <hpopal@bsosmwpsybps031.umd.edu>
2024-05-08 11:56:48 -0400

    only good runs

--- code/neuron_code/rl_1st_level-indiv_runs.py
+++ code/neuron_code/rl_1st_level-indiv_runs.py
@@ -16,16 +16,22 @@ from nilearn.glm.first_level import FirstLevelModel
 from nilearn.glm.first_level import compute_regressor
 from nilearn.glm.first_level import make_first_level_design_matrix
 
+from nilearn.plotting import plot_design_matrix
+from matplotlib import pyplot as plt
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
 ##########################################################################
 # Set up
 ##########################################################################
 
 # Take script inputs
-subj = 'sub-SCN'+str(sys.argv[1])
+#subj = 'sub-SCN'+str(sys.argv[1])
 task = 'SR'
 
 # For beta testings
-#subj = 'sub-SCN215'
+subj = 'sub-SCN101'
 #task = 'social'
 
 # Define fmriprep template space
@@ -57,18 +63,26 @@ print('Starting 1st-level analysis for '+subj)
 # Make participant-specific directory for output if it doesn't exist
 if not os.path.exists(outp_dir):
     os.makedirs(outp_dir)
+    
+    
+# Find QC data for participant
+qc_data = pd.read_csv(os.path.join(bids_dir, 'derivatives', 'participants_good.csv'))
+
+# Filter for only participant QC data
+subj_qc_data = qc_data[qc_data['participant_id'] == subj]
+
 
 # Find all of the preprocessed functional runs
-func_runs = [f for f in glob.glob(bids_dir + '/derivatives/fmriprep/'+subj+'/func/'+subj+'_task-'+task+'*space-'+template+'_desc-preproc_bold.nii.gz', recursive=True)]
-func_runs.sort()
-print('Number of functional runs for '+subj+': '+str(len(func_runs)))
+#func_runs = [f for f in glob.glob(bids_dir + '/derivatives/fmriprep/'+subj+'/func/'+subj+'_task-'+task+'*space-'+template+'_desc-preproc_bold.nii.gz', recursive=True)]
+#func_runs.sort()
+#print('Number of functional runs for '+subj+': '+str(len(func_runs)))
 
 # Grab subject's T1 as a mask to keep analysis in subject space
 subj_t1 = bids_dir+'derivatives/fmriprep/'+subj+'/anat/'+subj+'_space-'+template+'_label-GM_probseg_bin.nii.gz'
 
 # Find the task event files that are ready to become design matrices
-event_files = [f for f in glob.glob(bids_dir + '/derivatives/rl_modeling/SCN_'+subj[-3:]+'/'+subj+'_task-'+task+'_run-*_desc-events'+'.csv', recursive=True)]
-event_files.sort()
+#event_files = [f for f in glob.glob(bids_dir + '/derivatives/rl_modeling/SCN_'+subj[-3:]+'/'+subj+'_task-'+task+'_run-*_desc-events'+'.csv', recursive=True)]
+#event_files.sort()
 
 # Set path to subject specific fmriprep output
 fmri_run_data_dir = bids_dir+'derivatives/fmriprep/'+subj+'/func/'
@@ -78,8 +92,12 @@ motion_reg_names = ['trans_x','trans_y','trans_z','rot_x','rot_y','rot_z',
                     'trans_x_derivative1','trans_y_derivative1','trans_z_derivative1',
                     'rot_x_derivative1','rot_y_derivative1','rot_z_derivative1',
                     'white_matter','csf','scrub']
-confounds = []
+
+# Create empty lists to append run data
+func_runs = []
 events = []
+confounds = []
+design_matrices = []
 
 # Set the relevant conditions (not contrasts)
 relv_conds = ['RPE', 'RPE_abs', 'ButtonPress']
@@ -88,10 +106,25 @@ relv_conds = ['RPE', 'RPE_abs', 'ButtonPress']
 ##########################################################################
 # Loop through funcitonal runs
 ##########################################################################
-design_matrices = []
-for n in range(len(func_runs)):
+
+for n in range(len(subj_qc_data)):
+    
+    # Specify run number
+    run_num = subj_qc_data['run'].iloc[0][-1]
+    
+    # Find preprocessed functional run
+    func_run = bids_dir + '/derivatives/fmriprep/'+subj+'/func/'+subj+'_task-'+task+'_run-'+run_num+'_space-'+template+'_desc-preproc_bold.nii.gz'
+    func_runs.append(func_run)
+    
+    # Import the event file as a dataframe
+    event_path = os.path.join(bids_dir, 'derivatives', 'rl_modeling', 'event_files',
+                              'SCN_'+subj[-3:], subj+'_task-'+task+'_run-'+run_num+'_desc-events'+'.csv')
+    event_file = pd.read_csv(event_path)
+    event_file = event_file[event_file['trial_type'].str.contains('fixation') == False]
+    events.append(event_file[['trial_type','onset','duration']])
+    
     # Set motion parameters and input as a dataframe
-    motion_reg = pd.read_csv(fmri_run_data_dir+subj+'_task-'+task+'_run-'+str(n+1)+'_desc-confounds_timeseries.tsv', sep='\t')
+    motion_reg = pd.read_csv(fmri_run_data_dir+subj+'_task-'+task+'_run-'+run_num+'_desc-confounds_timeseries.tsv', sep='\t')
     
     # Add a regressor to "scrub" TRs with large framewise displacement
     motion_reg['scrub'] = 0.0
@@ -104,33 +137,32 @@ for n in range(len(func_runs)):
     # general confounds list
     confounds.append(motion_reg[motion_reg_names])
 
-    # Import the event file as a dataframe                 
-    temp_event_file = pd.read_csv(event_files[n])
-    temp_event_file = temp_event_file[temp_event_file['trial_type'].str.contains('fixation') == False]
-    events.append(temp_event_file[['trial_type','onset','duration']])
-    
-    
+
     # Calcualte RPE regressor
     #frame_times = np.linspace(0.625,tr*n_scans, n_scans)
     start_time = slice_time_ref * tr
     end_time = (n_scans - 1 + slice_time_ref) * tr
     frame_times = np.linspace(start_time, end_time, n_scans)
-    rpe_events = temp_event_file[temp_event_file['trial_type'].str.contains('fb')]
-    rpe_events.loc[:,'RPE_abs'] = list(rpe_events['RPE'].abs())
+    rpe_events = event_file[event_file['trial_type'].str.contains('fb')].copy()
+    rpe_events['RPE_abs'] = rpe_events['RPE'].abs().to_list()
     rpe_condition = rpe_events[['onset','duration','RPE']].to_numpy()
-    signal_rpe, _labels = compute_regressor(rpe_condition.T, 'spm', frame_times, con_id='RPE')
+    signal_rpe, _labels = compute_regressor(rpe_condition.T, 'spm', 
+                                            frame_times, con_id='RPE')
     rpe_abs_condition = rpe_events[['onset','duration','RPE_abs']].to_numpy()
-    signal_rpe_abs, _labels = compute_regressor(rpe_abs_condition.T, 'spm', frame_times, con_id='RPE_abs')
-    #test['RPE'] = signal
+    signal_rpe_abs, _labels = compute_regressor(rpe_abs_condition.T, 'spm', 
+                                                frame_times, con_id='RPE_abs')
+    
+    # Drop the RPE column in the event file
+    event_file_clean = event_file[['trial_type','onset','duration']]
     
     # Make design matrix
     design_matrix = make_first_level_design_matrix(frame_times, 
-                                                        events[n], 
-                                                        hrf_model='spm',
-                                                        drift_model='polynomial',
-                                                        high_pass=0.01,
-                                                        add_regs=confounds[n].values,
-                                                        add_reg_names=confounds[n].columns.tolist())
+                                                   event_file_clean, 
+                                                   hrf_model='spm',
+                                                   drift_model='polynomial',
+                                                   high_pass=0.01,
+                                                   add_regs=confounds[n].values,
+                                                   add_reg_names=confounds[n].columns.tolist())
     
     # Add RPE regressor
     design_matrix.loc[:,'RPE'] = signal_rpe
@@ -155,10 +187,16 @@ for n in range(len(func_runs)):
                                high_pass=0.01)
 
     # Conduct the GLM using the functional data, event file, and the confounds
-    fmri_glm = fmri_glm.fit(func_runs[n], design_matrices=design_matrix)
+    fmri_glm = fmri_glm.fit(func_run, design_matrices=design_matrix)
 
     # Specify the design matrix to pull conditions and contrasts later
     design_matrix = fmri_glm.design_matrices_[0]
+    
+    # Plot and save design matrix
+    plot_design_matrix(design_matrix)
+    plt.savefig(os.path.join(outp_dir, 
+                             'task-'+task+'_run-'+run_num+'_design_matrix.png'))
+    
     
     # Find the total number of conditions in the design matrix
     n_conds = len(design_matrix.columns)
@@ -178,11 +216,11 @@ for n in range(len(func_runs)):
     # Create z-scored beta maps contrasts
     for n_cont in range(len(contrasts)):
         cont_name = list(contrasts.keys())[n_cont]
-        z_map = fmri_glm.compute_contrast(contrasts[cont_name], output_type='z_score')
+        z_map = fmri_glm.compute_contrast(contrasts[cont_name], 
+                                          output_type='z_score')
     
-        z_map.to_filename(os.path.join(outp_dir,'zmap_'+task+'_'+cont_name+'_run-'+str(n+1)+'.nii.gz'))
-    
-    # Export design matrix image
+        z_map.to_filename(os.path.join(outp_dir,
+                                       'zmap_'+task+'_'+cont_name+'_run-'+run_num+'.nii.gz'))
     
     
     # Save design matrix for between run analysis
@@ -202,9 +240,11 @@ fmri_glm = fmri_glm.fit(func_runs, design_matrices=design_matrices)
 
 for n_cont in range(len(contrasts)):
     cont_name = list(contrasts.keys())[n_cont]
-    z_map = fmri_glm.compute_contrast(contrasts[cont_name], output_type='z_score')
+    z_map = fmri_glm.compute_contrast(contrasts[cont_name], 
+                                      output_type='z_score')
 
-    z_map.to_filename(os.path.join(outp_dir,'zmap_'+task+'_'+cont_name+'_run-all.nii.gz'))
+    z_map.to_filename(os.path.join(outp_dir,
+                                   'zmap_'+task+'_'+cont_name+'_run-all.nii.gz'))
     
 
 
